35cef4cb6ecc0f697264838b3d258d21adb7d736
Cintia I. Shinoda <shinoda.c.i@gmail.com>
2026-03-14 22:16:09 -0300

    update: ELT-531

---
+++ 4-Principios-Matematicos-e-Computacionais/teste.m
@@ -0,0 +1,23 @@
+% for i = 2:30
+%     disp('Hello World')
+% end
+
+
+
+% declarar variáveis
+r = 34;
+w = 6;
+y = 23;
+
+disp(pi)
+disp(j)
+disp(5j)
+
+% vetor
+y = 3;
+x = (2:30);
+disp(x);
+plot(x,y);
+
+
+close all;
